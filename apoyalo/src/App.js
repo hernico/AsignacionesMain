bbc838455a5a762798bc0a871d1ddf7a613082f1
hernico <hernico@gmail.com>
2023-03-05 14:43:41 -0500

    new
    new

---
+++ apoyalo/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+<h1>Doe, Jane</h1>
+<p>age: 45</p>
+<p>Hair Color: Black</p>
+
+<h1>Smith, John</h1>
+<p>age: 88</p>
+<p>Hair Color: Brown</p>
+
+<h1>Fillmore, Millard</h1>
+<p>age: 50</p>
+<p>Hair Color: Brown</p>
+
+<h1>Smith, Maria</h1>
+<p>age: 62</p>
+<p>Hair Color: Brown</p>
+
+
+    </div>
+  );
+}
+
+export default App;
