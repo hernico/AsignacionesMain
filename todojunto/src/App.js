3856271338533a1fb771d713dd3f58353ed838a3
hernico <hernico@gmail.com>
2023-03-05 21:27:39 -0500

    Gran inversion
    Se agregan 2 personas adicionales para la actividad de Gran inversion

--- todojunto/src/App.js
+++ todojunto/src/App.js
@@ -8,13 +8,15 @@ function App() {
       
   <div className="item1">Header</div>
   <div className="item2">
+    </div>
+  <div className="item3">Main
   <Personas firstName= {'Jane'} lastName={'Doe'} age={45} hairColor={'black'}  />
       <br />
-      <Personas firstName= {'Jane'} lastName={'Doe'} age={88} hairColor={'black'} />
-
+      <Personas firstName= {'John'} lastName={'Smith'} age={88} hairColor={'bBrownlack'} />
+      <Personas firstName= {'Millard'} lastName={'Fillmore'} age={50} hairColor={'Brown'} />
+      <Personas firstName= {'Maria'} lastName={'Smith'} age={62} hairColor={'Brown'} />
+      </div>  
     
-    </div>
-  <div className="item3">Main</div>  
   <div className="item4">Right</div>
   <div className="item5">Footer</div>
 </div>
